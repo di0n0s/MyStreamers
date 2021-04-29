14cf38fb266330004f334df0c318efeac300746c
di0n0s <sfuentescarrique@gmail.com>
2021-04-29 18:15:24 +0200

    Revert "Feature/create router"

--- core/src/main/java/com/dionos/core/navigation/Router.kt
+++
@@ -1,9 +0,0 @@
-package com.dionos.core.navigation
-
-import androidx.fragment.app.FragmentManager
-
-interface Router {
-    var supportFragmentManager: FragmentManager
-
-    fun showPendingFragment()
-}
