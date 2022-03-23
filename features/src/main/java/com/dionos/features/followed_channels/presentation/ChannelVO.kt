7cca7e8d81d7bc6a1fe032378ec7534f1dae8920
di0n0s <sfuentecarrique@gmail.com>
2022-03-23 15:13:58 +0100

    ChannelVO.kt done

---
+++ features/src/main/java/com/dionos/features/followed_channels/presentation/ChannelVO.kt
@@ -0,0 +1,9 @@
+package com.dionos.features.followed_channels.presentation
+
+data class ChannelVO(
+    val imagePath: String,
+    val name: String,
+    val title: String,
+    val category: String,
+    val viewers: String
+)
