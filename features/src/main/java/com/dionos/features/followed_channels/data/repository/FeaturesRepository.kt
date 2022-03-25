731ad8d03e5362945b4ce34e794f352df6ef087c
di0n0s <sfuentecarrique@gmail.com>
2022-03-25 15:52:25 +0100

    Create getFollowedStreamList function on repo

--- features/src/main/java/com/dionos/features/followed_channels/data/repository/FeaturesRepository.kt
+++ features/src/main/java/com/dionos/features/followed_channels/data/repository/FeaturesRepository.kt
@@ -1,6 +1,16 @@
 package com.dionos.features.followed_channels.data.repository
 
+import com.dionos.features.followed_channels.data.source.FeaturesNetworkDataSource
+import com.dionos.user.data.source.UserNetworkDataSource
 import javax.inject.Inject
 
-class FeaturesRepository @Inject constructor() {
+class FeaturesRepository @Inject constructor(
+    private val userNetworkDataSource: UserNetworkDataSource,
+    private val featuresNetworkDataSource: FeaturesNetworkDataSource
+) {
+
+    suspend fun getFollowedStreamList(cursor: String) {
+        val userId = userNetworkDataSource.getUserId()
+        featuresNetworkDataSource.getFollowedStreamList(userId = userId, cursor = cursor)
+    }
 }
